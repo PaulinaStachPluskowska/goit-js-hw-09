8fb94c4fd542be4d7ee27a244e46c68f25d98435
PaulinaStachPluskowska <paulina.stach@hotmail.com>
2023-01-19 14:26:20 +0100

    Update 01-color-switcher.js
    - task 9.1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+
+let intervalId = null;
+
+buttonStart.addEventListener('click', function() {
+    buttonStart.setAttribute('disabled', '');
+    buttonStop.removeAttribute('disabled', '');
+    bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+    intervalId = setInterval(function() {
+    bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', function() {
+    buttonStop.setAttribute('disabled', '');
+    clearInterval(intervalId);
+    intervalId = null;
+    buttonStart.removeAttribute('disabled', '');
+    //bodyColor.style.backgroundColor = '#fff'
+});
 
